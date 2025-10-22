85d0c9eddbffe5ac5cbbb47c86ca76b2678f02a8
yolagil <agil@peotic.com>
2025-10-22 16:09:48 -0400

    fix extension links

--- src/a2a/utils/proto_utils.py
+++ src/a2a/utils/proto_utils.py
@@ -140,6 +140,7 @@ class ToProto:
             task_id=message.task_id or '',
             role=cls.role(message.role),
             metadata=cls.metadata(message.metadata),
+            extensions=message.extensions or [],
         )
 
     @classmethod
