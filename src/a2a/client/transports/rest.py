1a973f0944f37916466ace39cd678318dec00255
sokoliva <sokolaj@google.com>
2025-12-05 16:52:29 +0000

    feat: Add Agent Card Signature support

--- src/a2a/client/transports/rest.py
+++ src/a2a/client/transports/rest.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Callable
 from typing import Any
 
 import httpx
@@ -368,6 +368,7 @@ class RestTransport(ClientTransport):
         *,
         context: ClientCallContext | None = None,
         extensions: list[str] | None = None,
+        signature_verifier: Callable[[AgentCard], None] | None = None,
     ) -> AgentCard:
         """Retrieves the agent's card."""
         modified_kwargs = update_extension_header(
@@ -375,9 +376,12 @@ class RestTransport(ClientTransport):
             extensions if extensions is not None else self.extensions,
         )
         card = self.agent_card
+
         if not card:
             resolver = A2ACardResolver(self.httpx_client, self.url)
             card = await resolver.get_agent_card(http_kwargs=modified_kwargs)
+            if signature_verifier is not None:
+                signature_verifier(card)
             self._needs_extended_card = (
                 card.supports_authenticated_extended_card
             )
@@ -395,6 +399,9 @@ class RestTransport(ClientTransport):
             '/v1/card', {}, modified_kwargs
         )
         card = AgentCard.model_validate(response_data)
+        if signature_verifier is not None:
+            signature_verifier(card)
+
         self.agent_card = card
         self._needs_extended_card = False
         return card
