0090ecc3b73c6eeda52c326af6261068ae7d0e40
sokoliva <sokolaj@google.com>
2026-02-20 17:22:30 +0000

    fix: fix unit test error

--- tests/server/request_handlers/test_jsonrpc_handler.py
+++ tests/server/request_handlers/test_jsonrpc_handler.py
@@ -550,11 +550,12 @@ class TestJSONRPCtHandler(unittest.async_case.IsolatedAsyncioTestCase):
             task_id=mock_task.id,
             config=push_config,
         )
-        response = await handler.set_push_notification_config(request)
+        context = ServerCallContext()
+        response = await handler.set_push_notification_config(request, context)
         self.assertIsInstance(response, dict)
         self.assertTrue(is_success_response(response))
         mock_push_notification_store.set_info.assert_called_once_with(
-            mock_task.id, push_config, None
+            mock_task.id, push_config, context
         )
 
     async def test_get_push_notification_success(self) -> None:
