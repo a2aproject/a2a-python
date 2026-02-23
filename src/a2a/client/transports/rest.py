b6eb1074ce22d26994069e2ceaf9f6df3c11cace
Ivan Shymko <ishymko@google.com>
2026-02-23 18:16:11 +0100

    refactor(client): remove URL resolution logic from transports (#732)
    Rely on the `ClientFactory` to resolve proper URL and do not duplicate
    logic in transports. `AgentCard` is still passed to transports as it's
    used for capabilities inspection.
    
    Make both `agent_card` and `url` mandatory, transports are mainly used
    from the `ClientFactory` and `| None` are likely non-breaking leftovers.
    
    Fixes #703

--- src/a2a/client/transports/rest.py
+++ src/a2a/client/transports/rest.py
@@ -10,7 +10,6 @@ from google.protobuf.json_format import MessageToDict, Parse, ParseDict
 from google.protobuf.message import Message
 from httpx_sse import SSEError, aconnect_sse
 
-from a2a.client.card_resolver import A2ACardResolver
 from a2a.client.errors import (
     A2AClientHTTPError,
     A2AClientJSONError,
@@ -34,9 +33,6 @@ from a2a.types.a2a_pb2 import (
     Task,
     TaskPushNotificationConfig,
 )
-from a2a.utils.constants import (
-    TransportProtocol,
-)
 from a2a.utils.telemetry import SpanKind, trace_class
 
 
@@ -50,37 +46,17 @@ class RestTransport(ClientTransport):
     def __init__(
         self,
         httpx_client: httpx.AsyncClient,
-        agent_card: AgentCard | None = None,
-        url: str | None = None,
+        agent_card: AgentCard,
+        url: str,
         interceptors: list[ClientCallInterceptor] | None = None,
         extensions: list[str] | None = None,
     ):
         """Initializes the RestTransport."""
-        if url:
-            self.url = url
-        elif agent_card:
-            for interface in agent_card.supported_interfaces:
-                if interface.protocol_binding in (
-                    TransportProtocol.HTTP_JSON,
-                    TransportProtocol.JSONRPC,
-                ):
-                    self.url = interface.url
-                    break
-            else:
-                raise ValueError(
-                    f'AgentCard does not support {TransportProtocol.HTTP_JSON} '
-                    f'or {TransportProtocol.JSONRPC}'
-                )
-        else:
-            raise ValueError('Must provide either agent_card or url')
-        if self.url.endswith('/'):
-            self.url = self.url[:-1]
+        self.url = url.removesuffix('/')
         self.httpx_client = httpx_client
         self.agent_card = agent_card
         self.interceptors = interceptors or []
-        self._needs_extended_card = (
-            agent_card.capabilities.extended_agent_card if agent_card else True
-        )
+        self._needs_extended_card = agent_card.capabilities.extended_agent_card
         self.extensions = extensions
 
     async def _apply_interceptors(
@@ -415,15 +391,6 @@ class RestTransport(ClientTransport):
         )
 
         card = self.agent_card
-
-        if not card:
-            resolver = A2ACardResolver(self.httpx_client, self.url)
-            card = await resolver.get_agent_card(
-                http_kwargs=modified_kwargs,
-                signature_verifier=signature_verifier,
-            )
-            self.agent_card = card
-            self._needs_extended_card = card.capabilities.extended_agent_card
 
         if not card.capabilities.extended_agent_card:
             return card
