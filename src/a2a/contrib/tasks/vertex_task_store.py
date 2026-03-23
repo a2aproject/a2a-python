61766b2c42345545ed0186be672a3e6040c40082
Gabor Feher <gfeher@google.com>
2026-03-23 13:27:54 +0000

    remove an unusued import

--- src/a2a/contrib/tasks/vertex_task_store.py
+++ src/a2a/contrib/tasks/vertex_task_store.py
@@ -4,7 +4,6 @@ import logging
 try:
     import vertexai
     from vertexai import types as vertexai_types
-    from google.genai import types as genai_types
     from google.genai import errors as genai_errors
 except ImportError as e:
     raise ImportError(
