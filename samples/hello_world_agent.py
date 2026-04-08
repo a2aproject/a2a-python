1010d4b2b86d2d4e3b7c0bf9980c638e9f220a63
guglielmoc <guglielmoc@google.com>
2026-04-08 09:51:55 +0000

    fix import

--- samples/hello_world_agent.py
+++ samples/hello_world_agent.py
@@ -13,9 +13,7 @@ from a2a.server.agent_execution.agent_executor import AgentExecutor
 from a2a.server.agent_execution.context import RequestContext
 from a2a.server.events.event_queue import EventQueue
 from a2a.server.request_handlers import GrpcHandler
-from a2a.server.request_handlers.default_request_handler import (
-    DefaultRequestHandler,
-)
+from a2a.server.request_handlers import DefaultRequestHandler
 from a2a.server.routes import (
     create_agent_card_routes,
     create_jsonrpc_routes,
