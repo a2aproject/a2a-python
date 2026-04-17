dfa9e563ae1a78322e553d85194ec64794f580df
guglielmoc <guglielmoc@google.com>
2026-04-17 12:15:49 +0000

    fix

--- scripts/test_minimal_install.py
+++ scripts/test_minimal_install.py
@@ -38,7 +38,6 @@ CORE_MODULES = [
     'a2a.client.client',
     'a2a.client.client_factory',
     'a2a.client.errors',
-    'a2a.client.helpers',
     'a2a.client.interceptors',
     'a2a.client.optionals',
     'a2a.client.transports',
